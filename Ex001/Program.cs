ed51f544f195e347777cd8ba8ce0d870bd3c0c4e
Anastasiya S <s9633128723@yandex.ru>
2023-04-15 14:51:13 +0300

    добавлено название задачи

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,4 +1,10 @@
-﻿int a = int.Parse(Console.ReadLine());
+﻿//Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
 
 if (a>b)
